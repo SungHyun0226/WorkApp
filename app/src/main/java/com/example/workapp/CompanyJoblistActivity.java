c8642b74e53893cad169781fcbc2fa59d52a091c
SungHyun0226 <tjdgusdksl@gmail.com>
2022-04-11 20:36:10 +0900

    1

--- app/src/main/java/com/example/workapp/CompanyJoblistActivity.java
+++ app/src/main/java/com/example/workapp/CompanyJoblistActivity.java
@@ -6,8 +6,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,42 +19,42 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class CompanyJoblistActivity extends AppCompatActivity {
-    private ArrayList<Recruit> RecruitList = new ArrayList<Recruit> (
+    private ArrayList<Recruit>  RecruitList = new ArrayList<Recruit> (
             Arrays.asList(
-                    new Recruit("company1", "A  we are now hiring good students!", "18만원","2022/04/10","충주시 단월동", "단순 작업인부 24352434237234243543643624352436", "2022/04/01"),
+                    new Recruit("company1", "A  we are now hiring bad students!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
                     new Recruit("company5", "E  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
                     new Recruit("company2", "B  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
-                    new Recruit("company3", "C  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
+                    new Recruit("company3", "C  we are looking for bad slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
                     new Recruit("company4", "D  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
                     new Recruit("company8", "H  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
-                    new Recruit("company6", "F  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
+                    new Recruit("company6", "F  we are looking for bad slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
                     new Recruit("company7", "G  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
-                    new Recruit("company11", "K  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
+                    new Recruit("company11", "K  we are looking for brilliant slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
                     new Recruit("company9", "I  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
-                    new Recruit("company10", "J  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
+                    new Recruit("company10", "J  we are looking for brilliant slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
                     new Recruit("company11", "K  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
-                    new Recruit("company12", "L  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
-                    new Recruit("company13", "M  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
+                    new Recruit("company12", "L  we are looking for bad slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
+                    new Recruit("company13", "M  we are looking for brilliant slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01"),
                     new Recruit("company14", "N  we are looking for good slaves!","18만원","2022/04/10","충주시 단월동", "단순 작업인부", "2022/04/01")
             )) ;
-
+    final private ArrayList<Recruit> RecruitList2 = new ArrayList<>(RecruitList);
     private RecruitListAdapter mAdapter;
     private RecyclerView mRecruits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_company_joblist);
+        setContentView(R.layout.activity_participant_joblist);
 
         mRecruits = (RecyclerView) findViewById(R.id.recruit_rcview);
 
-        mAdapter = new RecruitListAdapter(RecruitList);
+        mAdapter = new RecruitListAdapter(RecruitList2);
 
         mAdapter.setOnItemClickListener(new RecruitListAdapter.OnItemClickEventListener() {
             @Override
             public void onItemClick(View view, int pos) {
                 Intent myIntent = new Intent(CompanyJoblistActivity.this, RecruitActivity.class);
-                myIntent.putExtra("recruit", RecruitList.get(pos));
+                myIntent.putExtra("recruit", RecruitList2.get(pos));
                 startActivity(myIntent);
             }
         });
@@ -62,7 +66,7 @@ public class CompanyJoblistActivity extends AppCompatActivity {
         companySortButton.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Collections.sort(RecruitList, new Comparator<Recruit>(){
+                Collections.sort(RecruitList2, new Comparator<Recruit>(){
                     public int compare(Recruit obj1, Recruit obj2){
                         return obj1.getCompanyName().compareToIgnoreCase(obj2.getCompanyName());
                     }
@@ -71,16 +75,30 @@ public class CompanyJoblistActivity extends AppCompatActivity {
             }
         });
         Button titleSortButton= (Button) findViewById(R.id.bt_title);
-        titleSortButton.setOnClickListener(new Button.OnClickListener() {
+        titleSortButton.setOnClickListener(view -> {
+            Collections.sort(RecruitList2, (obj1, obj2) -> obj1.getTitle().compareToIgnoreCase(obj2.getTitle()));
+            mAdapter.notifyDataSetChanged();
+        });
+        EditText recruitSearch = findViewById(R.id.et_recruitsearch);
+        recruitSearch.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                Collections.sort(RecruitList, new Comparator<Recruit>(){
-                    public int compare(Recruit obj1, Recruit obj2){
-                        return obj1.getTitle().compareToIgnoreCase(obj2.getTitle());
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int count, int after) {
+                Log.d("edittextdebugging", s.toString());
+                String searchContent = s.toString();
+                RecruitList2.clear();
+                RecruitList.forEach(item -> {
+                    if (item.getTitle().contains(searchContent)) {
+                        RecruitList2.add(item);
                     }
                 });
                 mAdapter.notifyDataSetChanged();
             }
+            @Override
+            public void afterTextChanged(Editable s) {}
         });
     }
+
 }
